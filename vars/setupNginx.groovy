25d5f40ff1078495075a329d83712acc65319ff2
ruos-sovanra <vannraruos@gmail.com>
2024-10-16 11:49:53 +0700

    library for testing infra

--- vars/setupNginx.groovy
+++ vars/setupNginx.groovy
@@ -2,8 +2,10 @@ def call() {
     // Print the current working directory
     sh 'pwd'
 
-    // Specify the path to your Ansible playbook for Nginx
-    def playbookFile = 'ansible/playbooks/install_nginx.yml'
+    sh 'ls -lrt'
+
+    // Specify the absolute path to your Ansible playbook for Nginx
+    def playbookFile = '/opt/jenkins/workspace/remote-ansible/ansible/playbooks/install_nginx.yml'
 
     // Execute the Ansible playbook
     sh """
