976e588ee8e26868a1fae8d16ab2786ebd13dbb7
ruos-sovanra <vannraruos@gmail.com>
2024-10-17 08:51:26 +0700

    library for testing infra

--- src/org/devops/AnsibleExecutor.groovy
+++ src/org/devops/AnsibleExecutor.groovy
@@ -2,11 +2,11 @@ package org.devops
 
 def runPlaybook(playbook, inventoryFile = 'resources/ansible/nginx/hosts', extraVars = [:]) {
     def extraVarsString = extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')
-    def command = ["ansible-playbook", playbook, "-i", inventoryFile] + extraVarsString.split(' ')
+    def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString}"
 
     try {
         echo "Executing Ansible Playbook: ${playbook}"
-        sh(script: command.join(' '), returnStdout: true)
+        sh command
     } catch (Exception e) {
         error "Failed to execute Ansible Playbook: ${e.message}"
     }
