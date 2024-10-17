3b4f1165fdac14329ec6e0752244b3b8dd3a6215
ruos-sovanra <vannraruos@gmail.com>
2024-10-17 10:19:20 +0700

    library for testing infra

--- src/org/devops/NginxExecutor.groovy
+++ src/org/devops/NginxExecutor.groovy
@@ -2,37 +2,23 @@ package org.devops
 
 class NginxExecutor {
 
-    static void runNginxPlaybook(String playbook = '/opt/jenkins/workspace/remote-ansible/resources/ansible/playbooks/install_nginx.yml', String inventoryFile = 'resources/ansible/inventory/hosts', Map<String, String> extraVars = [:]) {
+    static void runPlaybook(String playbook, String inventoryFile = 'resources/ansible/nginx/hosts', Map<String, String> extraVars = [:]) {
         def extraVarsString = extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')
         def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString}"
 
-        println "Command to execute: ${command}"
-
         try {
-            println "Executing Ansible Playbook: ${playbook}"
-            def process = command.execute()
-            def output = new StringBuffer()
-            def error = new StringBuffer()
-            process.consumeProcessOutput(output, error)
-            process.waitFor()
-
-            println "Output: ${output}"
-            println "Error: ${error}"
-
-            if (process.exitValue() != 0) {
-                throw new RuntimeException("Failed to execute Ansible Playbook: ${playbook}\nError: ${error}")
-            }
+            echo "Executing Ansible Playbook: ${playbook}"
+            sh command
         } catch (Exception e) {
-            println "Error: ${e.message}"
-            throw e
+            error "Failed to execute Ansible Playbook: ${e.message}"
         }
     }
 
     static void installNginx() {
-        runNginxPlaybook('/opt/jenkins/workspace/remote-ansible/resources/ansible/playbooks/install_nginx.yml')
+        runPlaybook('resources/ansible/playbooks/install_nginx.yml')
     }
 
     static void uninstallNginx() {
-        runNginxPlaybook('resources/ansible/roles/nginx/tasks/uninstall.yml')
+        runPlaybook('resources/ansible/roles/nginx/tasks/uninstall.yml')
     }
 }
