fc67a40bc5adfa3057a064f190bc9036bb18a87a
ruos-sovanra <vannraruos@gmail.com>
2024-10-17 11:10:24 +0700

    library for testing infra

--- src/org/devops/NginxExecutor.groovy
+++ src/org/devops/NginxExecutor.groovy
@@ -2,15 +2,15 @@ package org.devops
 
 class NginxExecutor {
 
-    static void runPlaybook(String playbook, String inventoryFile = 'resources/ansible/nginx/hosts', Map<String, String> extraVars = [:]) {
+    // Method to run the Ansible playbook with optional extra variables and debug flag
+    static void runPlaybook(String playbook, String inventoryFile = 'resources/ansible/inventory/hosts', Map<String, String> extraVars = [:], boolean debug = false) {
         def playbookFile = new File(playbook)
-        println("Playbook file: ${playbookFile}")
         if (!playbookFile.exists()) {
             throw new RuntimeException("Playbook file not found: ${playbook}")
         }
 
         def extraVarsString = extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')
-        def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString}"
+        def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString} ${debug ? '-vvv' : ''}"
 
         try {
             println "Executing Ansible Playbook: ${playbook}"
@@ -20,8 +20,9 @@ class NginxExecutor {
             process.consumeProcessOutput(output, error)
             process.waitFor()
 
-            println "Output: ${output}"
-            println "Error: ${error}"
+            // Separate output for better log clarity
+            println "Ansible Output: ${output}"
+            println "Ansible Error: ${error}"
 
             if (process.exitValue() != 0) {
                 throw new RuntimeException("Failed to execute Ansible Playbook: ${playbook}\nError: ${error}")
@@ -31,11 +32,13 @@ class NginxExecutor {
         }
     }
 
-    static void installNginx() {
-        runPlaybook('resources/ansible/playbooks/install_nginx.yml')
+    // Method to install Nginx using the dedicated Ansible playbook
+    static void installNginx(boolean debug = false) {
+        runPlaybook('resources/ansible/playbooks/install_nginx.yml', 'resources/ansible/inventory/hosts', [:], debug)
     }
 
-    static void uninstallNginx() {
-        runPlaybook('resources/ansible/roles/nginx/tasks/uninstall.yml')
+    // Method to uninstall Nginx using the dedicated Ansible task
+    static void uninstallNginx(boolean debug = false) {
+        runPlaybook('resources/ansible/roles/nginx/tasks/uninstall.yml', 'resources/ansible/inventory/hosts', [:], debug)
     }
 }
