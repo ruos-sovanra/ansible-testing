92748d08608faf2cb45fb9cd3a4cab2cee0e3130
ruos-sovanra <vannraruos@gmail.com>
2024-10-17 09:21:41 +0700

    library for testing infra

---
+++ src/org/devops/NginxExecutor.groovy
@@ -0,0 +1,21 @@
+package org.devops
+
+class NginxExecutor {
+
+    static void runNginxPlaybook(String playbook = 'resources/ansible/playbooks/install_nginx.yml', String inventoryFile = 'resources/ansible/inventory/hosts', Map<String, String> extraVars = [:]) {
+        def extraVarsString = extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')
+        def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString}"
+
+        try {
+            println "Executing Ansible Playbook: ${playbook}"
+            def process = command.execute()
+            process.waitForProcessOutput(System.out, System.err)
+            if (process.exitValue() != 0) {
+                throw new RuntimeException("Failed to execute Ansible Playbook: ${playbook}")
+            }
+        } catch (Exception e) {
+            println "Error: ${e.message}"
+            throw e
+        }
+    }
+}
