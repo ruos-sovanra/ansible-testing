84d6765d2d006ecb5cfd13a8fa24a009b0feebd7
ruos-sovanra <vannraruos@gmail.com>
2024-10-17 09:53:47 +0700

    library for testing infra

--- src/org/devops/NginxExecutor.groovy
+++ src/org/devops/NginxExecutor.groovy
@@ -3,13 +3,16 @@ package org.devops
 class NginxExecutor {
 
     static void runNginxPlaybook(String playbook = 'resources/ansible/playbooks/install_nginx.yml', String inventoryFile = 'resources/ansible/inventory/hosts', Map<String, String> extraVars = [:]) {
+        def workspace = System.getenv('WORKSPACE')
+        def playbookPath = "${workspace}/${playbook}"
+        def inventoryPath = "${workspace}/${inventoryFile}"
         def extraVarsString = extraVars.collect { k, v -> "-e ${k}=${v}" }.join(' ')
-        def command = "ansible-playbook ${playbook} -i ${inventoryFile} ${extraVarsString}"
+        def command = "ansible-playbook ${playbookPath} -i ${inventoryPath} ${extraVarsString}"
 
         println "Command to execute: ${command}"
 
         try {
-            println "Executing Ansible Playbook: ${playbook}"
+            println "Executing Ansible Playbook: ${playbookPath}"
             def process = command.execute()
             def output = new StringBuffer()
             def error = new StringBuffer()
@@ -20,7 +23,7 @@ class NginxExecutor {
             println "Error: ${error}"
 
             if (process.exitValue() != 0) {
-                throw new RuntimeException("Failed to execute Ansible Playbook: ${playbook}\nError: ${error}")
+                throw new RuntimeException("Failed to execute Ansible Playbook: ${playbookPath}\nError: ${error}")
             }
         } catch (Exception e) {
             println "Error: ${e.message}"
